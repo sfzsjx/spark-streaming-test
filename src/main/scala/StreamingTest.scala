36c2d594e6671d7c4a5ea0cae673594b1bc981b3
sfzsjx <2590667609@qq.com>
2018-08-11 14:36:45 +0800

    spark streaming test

---
+++ src/main/scala/StreamingTest.scala
@@ -0,0 +1,18 @@
+
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{ Seconds, StreamingContext }
+
+object StreamingTest {
+  def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("StreamingTest").setMaster("local[3]")
+    val ssc = new StreamingContext(sparkConf,Seconds(2))
+
+    val lines = ssc.socketTextStream("master", 19999)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
+    wordCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+} 
